5abebc9f5989d39bbfc77dfa2c535e6808a9d274
Rich Comber <rich@elementalwebdesign.co.uk>
2017-06-29 22:30:48 +0100

    A bit more header setup

--- source/js/components/products/header.js
+++ source/js/components/products/header.js
@@ -9,8 +9,30 @@ export default class ProductsHeader extends Component {
     return (
       <SecondaryHeader>
         <Column>
-          <h3><FontAwesome name='angle-right' size='2x' /> Products</h3>
+          <h3 className='secondary-header__title'><FontAwesome name='angle-right' /> Products</h3>
         </Column>
+        <div className='header-meta'>
+          <a class='header-action-button'>
+            <FontAwesome name='plus' />
+            <span className='header-action-label'>Add</span>
+          </a>
+          <a class='header-action-button active'>
+            <FontAwesome name='shopping-cart' />
+            <span className='header-action-label'>Products</span>
+          </a>
+          <a class='header-action-button'>
+            <FontAwesome name='sitemap' />
+            <span className='header-action-label'>Categories</span>
+          </a>
+          <a class='header-action-button'>
+            <FontAwesome name='tag' />
+            <span className='header-action-label'>Tags</span>
+          </a>
+          <a class='header-action-button'>
+            <FontAwesome name='arrows-h' />
+            <span className='header-action-label'>Dimensions</span>
+          </a>
+        </div>
       </SecondaryHeader>
     );
   }
