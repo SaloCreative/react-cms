a1b0f569d04369ce35dbd9e2d60f5709aa3ea6bb
Rich Comber <rich@elementalwebdesign.co.uk>
2017-08-02 23:16:56 +0100

    Setting up some config

--- source/js/components/products/description.js
+++ source/js/components/products/description.js
@@ -4,14 +4,31 @@ import { Helmet } from 'react-helmet';
 import FroalaEditor from 'react-froala-wysiwyg'
 import { Column, Row, Card } from 'components/core/grid';
 
+const config = {
+  fullPage: false,
+  charCounterCount: false,
+  placeholderText: 'Edit Your Content Here!',
+  fontFamilySelection: false,
+  inlineStyles: false,
+  toolbarButtons: [
+    'bold', 'italic', 'underline', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'insertTable', 'clearFormatting', 'html'
+  ],
+  height:300
+};
+
 export default class ProductDescription extends Component {
 
   render() {
     return (
       <Column classes='is-4'>
         <Card>
-          Description
-          <FroalaEditor tag='textarea'/>
+          <h4>Description</h4>
+          <div className='froala-wrapper'>
+            <FroalaEditor
+              config={config}
+              tag='textarea'
+            />
+          </div>
         </Card>
       </Column>
     );
