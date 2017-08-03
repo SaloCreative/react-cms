0ebb09ca37ce797fb5ae1223520024d5978f1a6a
Rich Comber <rich@elementalwebdesign.co.uk>
2017-08-03 22:12:08 +0100

    Fixing featured image size and styling up description block

--- source/js/components/products/description.js
+++ source/js/components/products/description.js
@@ -10,6 +10,7 @@ const config = {
   placeholderText: 'Edit Your Content Here!',
   fontFamilySelection: false,
   inlineStyles: false,
+  quickInsert: false,
   toolbarButtons: [
     'bold', 'italic', 'underline', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'insertTable', 'clearFormatting', 'html'
   ],
@@ -20,9 +21,8 @@ export default class ProductDescription extends Component {
 
   render() {
     return (
-      <Column classes='is-4'>
-        <Card>
-          <h4>Description</h4>
+      <Column classes='is-8'>
+        <Card classes='froala-card'>
           <div className='froala-wrapper'>
             <FroalaEditor
               config={config}
